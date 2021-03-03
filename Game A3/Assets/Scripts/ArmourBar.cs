af3c7ccb2bb9b57be9d0c4bcc16ddc37d02da556
Taylor Bromley <taylorbromley@sky.com>
2021-03-03 14:14:53 +0000

    Removed commented out code from script

--- Game A3/Assets/Scripts/ArmourBar.cs
+++ Game A3/Assets/Scripts/ArmourBar.cs
@@ -7,22 +7,18 @@ using UnityEngine.UI;
 public class ArmourBar : MonoBehaviour
 {
     public Slider slider;
-    //public Gradient gradient;
     public Image fill;
 
-    //could change this whenever they level up, increase armour etc?
     public void SetMaxArmour(int armour)
     {
         slider.maxValue = armour;
         slider.value = armour;
 
-        //fill.color = gradient.Evaluate(1f);
     }
 
     public void SetArmour(int armour)
     {
         slider.value = armour;
 
-        //fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
